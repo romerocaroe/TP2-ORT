9d169078976efeb90026b5c505c641a78fab7724
xcxr12 <CRomero2@pan-energy.com>
2024-05-20 21:30:44 -0300

    Manejo de la data obtenida

--- src/models/pokemons.models.js
+++ src/models/pokemons.models.js
@@ -2,9 +2,9 @@ import fetch from 'node-fetch';
 
 const getPokemons = async () => {
     try {
-        const res = await fetch(process.env.POKE_API+'?limit=100');
-        const json = await res.json();
-        console.log(json);
+        const res = await fetch(process.env.POKE_API+'?limit=100')
+        const json = await res.json().results.map(pokemon => pokemon.name)
+        console.log(json)
         return json
     } catch(err){
         console.error(err)
@@ -14,10 +14,16 @@ const getPokemons = async () => {
 
 const getPokemonById = async (id) => {
     try {
-        const res = await fetch(process.env.POKE_API+id);
-        const json = await res.json();
-        console.log(json);
-        return json
+        const res = await fetch(process.env.POKE_API+id)
+        const json = await res.json()
+        const pokemon = {
+            "id": json.id,
+            "nombre": json.name,
+            "altura": json.height,
+            "peso": json.weight
+        }
+        console.log(pokemon);
+        return pokemon
     } catch(err){
         console.error(err)
         return err
